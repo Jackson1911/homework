ccae91f4e42f7f59e57159ede7e729ad780a6b04
Jackson1911 <7witcher791@mail.ru>
2016-09-16 17:14:28 +0600

    Update create.php

--- views/news/create.php
+++ views/news/create.php
@@ -19,13 +19,13 @@
 			<div>
 				<p>Для добавления новости заполните следующие поля и нажмите на кнопку "Добавить":</p>
 				<label>Заголовок: <br>
-					<input type="text" name="name">
+					<input type="text" name="name" required autofocus>
 				</label><br>
 				<label>Дата публикации: <br>
-					<input type="date" name="date">
+					<input type="date" name="date" required>
 				</label><br>
 				<label>Содержимое: <br>
-					<textarea name="content"></textarea>
+					<textarea name="content" required></textarea>
 				</label><br>
 				<input type="submit" name="submit" value="Добавить">
 			</div>
