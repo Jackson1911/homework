3f6bbc580bf553d51582586bd5d0efc2cedcb205
Jackson1911 <7witcher791@mail.ru>
2016-09-21 12:37:32 +0600

    Update index.php

--- views/news/index.php
+++ views/news/index.php
@@ -2,7 +2,7 @@
 	<div class="mainstyle">
 		<h3><a href="/news/view?id=<?= $value->id; ?>"><?= $value->title; ?></a></h3>
 		<em>Опубликовано: <?= $value->date; ?></em>
-		<p><?= cutStr($value->content); ?></p>
+		<p><?= $value->content; ?></p>
 	</div>	
 <?php endforeach ?>
 
