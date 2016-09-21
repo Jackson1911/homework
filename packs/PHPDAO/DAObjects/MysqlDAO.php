27d05010d37f6bcc4eac4008959d0e40e3492fc2
Jackson1911 <7witcher791@mail.ru>
2016-09-21 09:09:56 +0600

    Update MysqlDAO.php

--- packs/PHPDAO/DAObjects/MysqlDAO.php
+++ packs/PHPDAO/DAObjects/MysqlDAO.php
@@ -117,6 +117,19 @@ class MysqlDAO implements GeneralDAO
 
 		return $this;
 	}
+	
+	/**
+	 * @author Jackson1911
+	 * @param  string $columns
+	 * @return \packs\PHPDAO\DAObjects\GeneralDAO
+	 * Method for order data by field name
+	 */
+	public function orderBy(string $fieldName):GeneralDAO
+	{
+		$this->query['orderBy'] = $fieldName;
+
+		return $this;
+	}
 
 	/**
 	 * @author farZa
@@ -274,7 +287,12 @@ class MysqlDAO implements GeneralDAO
 
 		$sql = 'SELECT ' . $this->query['select'] . ' FROM ' . $this->query['table'];
 		$params = [];
+		
+		if (isset($this->query['orderBy'])){
 
+			$sql .= ' ORDER BY ' . $this->query['orderBy'];
+
+		}
 
 		if (isset($this->query['where'])) {
 			$whereResult = $this->generateValues('where');
