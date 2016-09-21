507ca4c4ea96d131dc6036bde4ea9dcdd785d0bd
Jackson1911 <7witcher791@mail.ru>
2016-09-21 12:15:29 +0600

    Update index.php

--- views/news/index.php
+++ views/news/index.php
@@ -6,21 +6,3 @@
 	</div>	
 <?php endforeach ?>
 
-<?php
-/**
- * [cutStr Функция которая обрезает строку, при этом не обрезая слова]
- * @param  string  $str     [Сюда передается строка которую нужно обрезать]
- * @param  integer $length  [Указываем сколько символов оставить в строке, по умолчанию 300]
- * @param  string  $postfix [Постфикс, окончание строки, по умолчанию: ...]
- * @return string           [Возвращает обработанную строку]
- */
-function cutStr($str, $length = 300, $postfix = '...')
-{
-    if (strlen($str) <= $length)
-        return $str;
- 
-    $temp = substr($str, 0, $length);
-    return substr($temp, 0, strrpos($temp, ' ') ) . $postfix;
-}
-?>
-
