e012407935630940d7a3864975fb029b2f402d01
Jackson1911 <7witcher791@mail.ru>
2016-09-21 10:39:41 +0600

    Update index.php

--- views/news/index.php
+++ views/news/index.php
@@ -1,8 +1,8 @@
 <?php foreach ($data as $value): ?>
 	<div class="mainstyle">
-		<h3><a href="/news/view?id=<?= $value['id']; ?>"><?= $value['title']; ?></a></h3>
-		<em>Опубликовано: <?= $value['date']; ?></em>
-		<p><?= cutStr($value['content']); ?></p>
+		<h3><a href="/news/view?id=<?= $value->id; ?>"><?= $value->title; ?></a></h3>
+		<em>Опубликовано: <?= $value->date; ?></em>
+		<p><?= cutStr($value->content); ?></p>
 	</div>	
 <?php endforeach ?>
 
