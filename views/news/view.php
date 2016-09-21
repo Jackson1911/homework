6b1207d0a81b97936355e95e259d67e74eaece79
Jackson1911 <7witcher791@mail.ru>
2016-09-21 10:41:00 +0600

    Update view.php

--- views/news/view.php
+++ views/news/view.php
@@ -3,9 +3,9 @@
 	<hr>
 	<form action="/news/view?id=<?= $_GET['id']; ?>">
 		<div>
-			<h3><?= $data['title']; ?></h3>
-			<em>Опубликовано: <?= $data['date']; ?></em>
-			<p><?= $data['content']; ?></p>
+			<h3><?= $data->title; ?></h3>
+			<em>Опубликовано: <?= $data->date; ?></em>
+			<p><?= $data->content; ?></p>
 			<hr>
 			<a href="/news/index">Вернуться на главную</a>
 		</div>
