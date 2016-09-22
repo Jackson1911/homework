6c44b7809de11fe2cdd3bbc151c7e4054cb1dbb9
Jackson1911 <7witcher791@mail.ru>
2016-09-22 12:13:22 +0600

    Update newsController.php

--- controllers/newsController.php
+++ controllers/newsController.php
@@ -8,6 +8,22 @@
 	class newsController extends SystemController
 	{
 		/**
+		 * [cutStr Функция которая обрезает строку, при этом не обрезая слова]
+		 * @param  string  $str     [Сюда передается строка которую нужно обрезать]
+		 * @param  integer $length  [Указываем сколько символов оставить в строке, по умолчанию 300]
+		 * @param  string  $postfix [Постфикс, окончание строки, по умолчанию: ...]
+		 * @return string           [Возвращает обработанную строку]
+		 */
+		function cutStr($str, $length = 300, $postfix = '...')
+		{
+			if (strlen($str) <= $length)
+			    return $str;
+			 
+			$temp = substr($str, 0, $length);
+			return substr($temp, 0, strrpos($temp, ' ') ) . $postfix;
+		}
+			
+		/**
 		 * [actionIndex - рендерит представление и выводит список всех новостей]
 		 * @var $data - содержит данные выбранные в БД
 		 */
@@ -16,26 +32,10 @@
 			$model = new News();
 			$data = $model->findAll();
 			
-			/**
-			 * [cutStr Функция которая обрезает строку, при этом не обрезая слова]
-			 * @param  string  $str     [Сюда передается строка которую нужно обрезать]
-			 * @param  integer $length  [Указываем сколько символов оставить в строке, по умолчанию 300]
-			 * @param  string  $postfix [Постфикс, окончание строки, по умолчанию: ...]
-			 * @return string           [Возвращает обработанную строку]
-			 */
-			function cutStr($str, $length = 300, $postfix = '...')
-			{
-			    if (strlen($str) <= $length)
-			        return $str;
-			 
-			    $temp = substr($str, 0, $length);
-			    return substr($temp, 0, strrpos($temp, ' ') ) . $postfix;
-			}
-
 			//Перебираем $data и обрезаем строки
 			foreach ($data as $value) 
 			{
-				$value->content = cutStr($value->content);	
+				$value->content = self::cutStr($value->content);	
 			}
 
 			/**
