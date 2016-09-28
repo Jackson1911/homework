f8cb32776b9ca978ddda0834877a096211217ca6
Jackson1911 <7witcher791@mail.ru>
2016-09-28 19:08:06 +0600

    Add files via upload

--- views/layout/default.php
+++ views/layout/default.php
@@ -3,27 +3,32 @@
 <head>
 	<title>Новостной сайт</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
 	<script type="text/javascript" src="/assets/js/jquery-3.1.1.js"></script>
 	<script type="text/javascript" src="/assets/js/script.js"></script>
 </head>
 <body>
-	<div class="block" style="opacity: 0.7; background: grey; position: fixed; width:100%; height: 100%; display: none;"></div>
-	<div class="header">
-		<div class="header-container">
-			<h2><a href="/news/index">Новостной сайт</a>
-			</h2><div class="admin"><a href="/news/newsadmin">Управление новостями</a></div>
+	<div class="block"></div>
+	<div class="header container-fluid">
+		<div class="row">
+			<div class=" nav container">
+				<div class="col-md-1"><a href="/news/index"><span class="glyphicon glyphicon-globe"></span> News</a></div>
+				<div class="col-md-3"><a href="/news/newsadmin"><span class="glyphicon glyphicon-user"></span> Управление новостями</a></div>
+			</div>
 		</div>
-	</div>
-	<div class="container">
-		<?php echo $content; ?>
-	</div>
-	<div class="error_box">
-		<div class="error_msg"></div>	
-	</div>
-	<div class="success_box">
-		<div class="success_msg"></div>	
-	</div>
-
+		</div>
+		<div class="container">
+			<div class="content">
+				<?php echo $content; ?>
+			</div>
+		</div>
+		
+		<div class="error_box">
+			<div class="error_msg"></div>	
+		</div>
+		<div class="success_box">
+			<div class="success_msg"></div>	
+		</div>
 </body>
 </html>
