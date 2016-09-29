edd7eec6e756ebd73f4a891dc472f262900f5512
Jackson1911 <7witcher791@mail.ru>
2016-09-29 20:14:31 +0600

    Update default.php

--- views/layout/default.php
+++ views/layout/default.php
@@ -6,18 +6,28 @@
 	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
 	<script type="text/javascript" src="/assets/js/jquery-3.1.1.js"></script>
+	<script type="text/javascript" src="/assets/js/bootstrap.js"></script>
+	<script type="text/javascript" src="/assets/js/bootstrap-confirmation.js"></script>
 	<script type="text/javascript" src="/assets/js/script.js"></script>
 </head>
 <body>
 	<div class="block"></div>
-	<div class="header container-fluid">
-		<div class="row">
-			<div class=" nav container">
-				<div class="col-md-1"><a href="/news/index"><span class="glyphicon glyphicon-globe"></span> News</a></div>
-				<div class="col-md-3"><a href="/news/newsadmin"><span class="glyphicon glyphicon-user"></span> Управление новостями</a></div>
-			</div>
-		</div>
-		</div>
+
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="/news/index"><span class="glyphicon glyphicon-globe"></span> News</a>
+					</div>
+					<div class="navbar-inner">	
+						<ul class="nav navbar-nav">
+							<li><a href="/news/registration"><span class="glyphicon glyphicon-user"></span> Регистрация</a></li>
+							<li><a href="/news/newsadmin"><span class="glyphicon glyphicon-user"></span> Управление новостями</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+
 		<div class="container">
 			<div class="content">
 				<?php echo $content; ?>
@@ -29,6 +39,11 @@
 		</div>
 		<div class="success_box">
 			<div class="success_msg"></div>	
+		</div>
+		<div class="confirm_box">
+			<div class="confirm_msg"></div>
+			<button id="confirm">Подтвердить</button>
+			<button id="cancel">Отмена</button>
 		</div>
 </body>
 </html>
