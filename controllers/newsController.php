99522f45677e2877b3b783bcd2403ded4669aed1
Jackson1911 <7witcher791@mail.ru>
2016-09-30 14:22:29 +0600

    Add files via upload

--- controllers/newsController.php
+++ controllers/newsController.php
@@ -193,26 +193,35 @@ class newsController extends SystemController
 		}		
 	}
 
+	/**
+	 * [actionRegistration - рендер представления регистрации нового пользователя]
+	 */
 	public function actionRegistration(){
 
 		CView::render('registration');
 
 	}
 
+	/**
+	 * [actionRegistrationProcess - добавление данных с формы в БД]
+	 */
 	public function actionRegistrationProcess(){
 
+		//Создание коротких переменных
 		$login = $_POST['login'];
 		$password = $_POST['pass1'];
 		$passwordAccept = $_POST['pass2'];
 		$email = $_POST['email'];
 
+		//Сравниваем пароли
 		if ($password != $passwordAccept) {
-			echo json_encode(['status' => 'err', 'message' => 'Ошибка. Пароли не совпадают']);
 			die();
 		}
 
+		//Шифрование пароля с помощью алгоритма md5
 		$hash = md5($password);
 
+		//Запрос к БД
 		$model = new Users();
 		$model->login = $login;
 		$model->password = $hash;
