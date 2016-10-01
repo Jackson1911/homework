b3a21cb55aeb8c8f7883837cc89be6e8407c4d75
Jackson1911 <7witcher791@mail.ru>
2016-10-01 09:42:48 +0600

    Upload users directory replace registration.php

---
+++ views/users/registration.php
@@ -0,0 +1,32 @@
+<form class="forms form-horizontal" role="form" onsubmit="ajaxUsersRegistration(event)">
+	<h3>Регистрация пользователей</h3>
+	<small>Для успешной регистрации заполните все поля и нажмите "Завершить регистрацию"</small>
+	<hr>
+	<div class="form-group">
+		<label class="col-sm-4 control-label">Логин:</label>
+		<div class="col-md-6">
+			<input class="form-control" type="text" name="login" required autofocus>
+		</div>
+	</div>
+	<div class="password form-group">
+		<label class="col-sm-4 control-label">Пароль:</label>
+		<div class="col-md-6">
+			<input id="pass1" class="form-control" type="password" name="pass1" oninput="passwordCompare()" required>
+			<span id="helpBlock1" class="help-block"></span>
+		</div>
+	</div>
+	<div class="passwordAccept form-group">
+		<label class="col-sm-4 control-label">Повторите пароль:</label>
+		<div class="col-md-6">
+			<input id="pass2" class="form-control" type="password" name="pass2" oninput="passwordCompare()" required>
+			<span id="helpBlock2" class="help-block"></span>
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="col-sm-4 control-label">Адрес электронной почты (e-mail):</label>
+		<div class="col-md-6">
+			<input class="form-control" type="text" name="email" required>
+		</div>
+	</div>
+	<input class="btn btn-success form-control" type="submit" value="Завершить регистрацию">
+</form>
